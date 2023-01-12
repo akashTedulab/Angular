8b38ae7a2380d0a0fef541694cc99137a420ecf7
akashTedulab <akashtayde@edulab.in>
2023-01-12 15:17:58 +0530

    For-Loop-ngFor

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  color = 'green'
+  friends = ["Eren", "Gon", "Killua", "Luffy", "Zoro", "Sanji"]
+
+  charDetails = [
+    {name:"Gon", age:12, anime:"Hunter X Hunter", otherChar:["Killua", "Gin", "Kurapika", "Leorio"]},
+    {name:"Luffy", age:19, anime:"One Piece", otherChar:["Zoro", "Sanji", "Chopper", "ussop"]},
+    {name:"Eren", age:25, anime:"Attack on Titan", otherChar:["Mikasa", "Levi", "Armoured Titan"]},
+    {name:"Thorphin", age:17, anime:"Vinland Saga", otherChar:["Thor", "Thorkill", "Askeladd"]}
+  ]
 }
