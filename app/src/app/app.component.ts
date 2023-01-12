3328f0e8cb33da6a4ff1c48b57c24d4715a9b67f
akashTedulab <120647560+akashTedulab@users.noreply.github.com>
2023-01-12 15:38:22 +0530

    Update app.component.ts

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -6,13 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  friends = ["Eren", "Gon", "Killua", "Luffy", "Zoro", "Sanji"]
-
-  charDetails = [
-    {name:"Gon", age:12, anime:"Hunter X Hunter", otherChar:["Killua", "Gin", "Kurapika", "Leorio"]},
-    {name:"Luffy", age:19, anime:"One Piece", otherChar:["Zoro", "Sanji", "Chopper", "ussop"]},
-    {name:"Eren", age:25, anime:"Attack on Titan", otherChar:["Mikasa", "Levi", "Armoured Titan"]},
-    {name:"Thorphin", age:17, anime:"Vinland Saga", otherChar:["Thor", "Thorkill", "Askeladd"]}
-  ]
+  title = 'Angular Tutorials';
+  color = 'red';
+  bgColor = 'blue';
+  center = 'center'
+  myFun(){
+    this.color = 'blue';
+    this.bgColor = 'red'
+  }
 }
