8bd3eaf6781616e25f93f365d4e7a4d224fd6088
akashTedulab <akashtayde@edulab.in>
2023-01-12 15:33:06 +0530

    Style-Binding-Dynamic-Style

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -7,12 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  friends = ["Eren", "Gon", "Killua", "Luffy", "Zoro", "Sanji"]
-
-  charDetails = [
-    {name:"Gon", age:12, anime:"Hunter X Hunter", otherChar:["Killua", "Gin", "Kurapika", "Leorio"]},
-    {name:"Luffy", age:19, anime:"One Piece", otherChar:["Zoro", "Sanji", "Chopper", "ussop"]},
-    {name:"Eren", age:25, anime:"Attack on Titan", otherChar:["Mikasa", "Levi", "Armoured Titan"]},
-    {name:"Thorphin", age:17, anime:"Vinland Saga", otherChar:["Thor", "Thorkill", "Askeladd"]}
-  ]
+  color = 'red';
+  bgColor = 'blue';
+  center = 'center'
+  myFun(){
+    this.color = 'blue';
+    this.bgColor = 'red'
+  }
 }
