e71d9bf907a850b95c6a87f3168200790c00744a
akashTedulab <akashtayde@edulab.in>
2023-01-13 11:15:09 +0530

    Todo-list-in-angular

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -5,7 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
- title = 'Angular Tutorials'
+ title = 'Angular Tutorials';
+ list:any[] = []
+ addTask(val:string){
+  this.list.push({id:this.list.length, name:val})
+ }
+ deleteItem(id:number){
+  this.list = this.list.filter( item => item.id != id)
+  console.log(id)
+ }
 }
 
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
