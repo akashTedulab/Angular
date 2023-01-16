ac2eee1e2d0749f8ed999761991c91dbb22784ce
akashTedulab <akashtayde@edulab.in>
2023-01-16 17:29:53 +0530

    404-Page-Wild-Card

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { NoPageComponent } from './no-page/no-page.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
@@ -14,8 +15,16 @@ const routes: Routes = [
     component: AboutComponent
   },
   {
+    path: 'user',
+    component: UserComponent
+  }, 
+  {
     path: 'user/:id',
     component: UserComponent
+  }, 
+  {
+    path: '**',
+    component:NoPageComponent
   }
 ];
 
