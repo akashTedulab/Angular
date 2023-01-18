70707d9b294de0e61f795376a32449e829f3b3f9
akashTedulab <akashtayde@edulab.in>
2023-01-18 13:47:50 +0530

    Modules and Routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './users/login/login.component';
+import { SignupComponent } from './users/signup/signup.component';
 
 const routes: Routes = [
-
+  {path: '', component: AppComponent},
+  {path:'login', component: LoginComponent},
+  {path:'signup', component: SignupComponent},
 ];
 
 @NgModule({
--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { dataType } from './interface';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +9,5 @@ import { dataType } from './interface';
 export class AppComponent {
  title = 'Angular Tutorials';
  title2 = "Model and Interface";
-
- getData(){
-  const data:dataType = {
-    name: 'akash',
-    id: 10,
-    indian: true,
-    address: '123 badlapur'
-  }
-  return data;
- }
 }
 
